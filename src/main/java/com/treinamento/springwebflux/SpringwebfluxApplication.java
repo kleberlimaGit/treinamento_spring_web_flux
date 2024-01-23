ad85cc8b16916dab1af31b46bb73fcc8c86c03d2
kleberlimagit <mov_kleber@hotmail.com>
2024-01-23 12:10:49 -0300

    inicio projeto

---
+++ src/main/java/com/treinamento/springwebflux/SpringwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxApplication.class, args);
+	}
+
+}
